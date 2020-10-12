fc416b2434c12a0948b6ceb4e3c982a1f68a45b7
Rob van den Berge <robvdberge@gmail.com>
2020-10-12 15:01:11 +0200

    added MessageService

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Heroes } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]>{
+    this.messageService.add('MessageService: Heroes are fetched!');
+    return of(Heroes);
+  }
+}
